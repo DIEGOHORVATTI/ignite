e6cdf2b1678168563ea782e39eb8675dd1904aa1
DIEGOHORVATTI <d.horvattid@gmail.com>
2023-05-11 08:59:56 -0300

    refactor: add preetier and eslint

--- aula-1/.eslintrc.cjs
+++ aula-1/.eslintrc.cjs
@@ -7,8 +7,29 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "no-loops",],
   rules: {
+    "no-console": [
+      "warn",
+      {
+        "allow": ["warn", "error"]
+      }
+    ],
     'react-refresh/only-export-components': 'warn',
+    "import/no-webpack-loader-syntax": "off",
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-img-element": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/no-children-prop": "off",
+    "no-use-before-define": "off",
+    "react/display-name": "off",
+    "react/prop-types": "off",
+    "no-unused-vars": "off",
+    "react/jsx-key": "off",
+    "prettier/prettier": 2,
+    "no-loops/no-loops": 2,
+    "no-undef": "warn",
+    "camelcase": "off",
+    "eqeqeq": "off"
   },
 }
